8208f60a294446cd211cac9e405bf5cf32a1d98d
MehfoozurRehman <mehfoozijaz786@gmail.com>
2024-06-01 12:52:02 +0500

    complete

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import App from "./App.jsx";
+import ErrorBoundary from "./Error.jsx";
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ErrorBoundary
+      fallback={(error, errorInfo) => (
+        <>
+          <h1>Something went wrong.</h1>
+          <details style={{ whiteSpace: "pre-wrap" }}>
+            {error && error.toString()}
+            <br />
+            {JSON.stringify(errorInfo?.componentStack || {}, null, 2)}
+          </details>
+        </>
+      )}
+    >
+      <App />
+    </ErrorBoundary>
+  </React.StrictMode>
+);
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,16 +1,45 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
+import { Hono } from "hono";
+import { cors } from "hono/cors";
+import { faker } from "@faker-js/faker";
+import { logger } from "hono/logger";
+import { serve } from "@hono/node-server";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.use(cors());
+app.use(logger());
 
-const port = 3000
-console.log(`Server is running on port ${port}`)
+const listOfUsers = (length: number) => {
+  return Array.from({ length }, (_, index) => ({
+    id: index + 1,
+    name: faker.person.fullName(),
+    email: faker.internet.email(),
+    picture: faker.image.avatar(),
+  }));
+};
+
+const users = listOfUsers(1000);
+
+app.get("/", async (c) => {
+  const { page = 1, limit = 10 } = c.req.query();
+
+  const skip: number = ((Number(page) - 1) * Number(limit)) as number;
+  const numberOfItems = skip + Number(limit);
+
+  const data = users.slice(skip, numberOfItems);
+
+  const totalPages = Math.ceil(users.length / Number(limit));
+
+  return c.json({
+    data,
+    totalPages,
+  });
+});
+
+const port = 3000;
+console.log(`Server is running on port ${port}`);
 
 serve({
   fetch: app.fetch,
-  port
-})
+  port,
+});
